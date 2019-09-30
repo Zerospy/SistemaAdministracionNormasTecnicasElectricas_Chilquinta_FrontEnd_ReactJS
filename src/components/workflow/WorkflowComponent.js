eddeca77051cca0b30edc59216eae0b8c7284e4e
Guillmar <guillmar.ortiz@gmail.com>
2019-09-30 11:40:42 -0300

    Agregando traducciones y definición de grid

--- src/components/workflow/WorkflowComponent.js
+++ src/components/workflow/WorkflowComponent.js
@@ -4,6 +4,9 @@ import {Col, Row} from 'mdbreact';
 import PropTypes from 'prop-types';
 import React from 'react';
 import {FormattedMessage, injectIntl} from 'react-intl';
+import Constantes from 'Constantes';
+import PanelComponent from 'components/commons/panels/PanelComponent';
+import DataGridComponent from 'components/commons/DataGrid/DataGridComponent';
 
 class WorkflowComponent extends React.Component {
     showSettings(event) {
@@ -13,8 +16,39 @@ class WorkflowComponent extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
+        const columnDefs = [];
+        columnDefs.push(
+            {
+                headerName: `${props.intl.formatMessage({
+                    id: 'component.workflow.datagrid.id'
+                })}`,
+                field: 'Id',
+                width: 70
+            },
+            {
+                headerName: `${props.intl.formatMessage({
+                    id: 'component.workflow.datagrid.name'
+                })}`,
+                field: 'Name',
+                width: 120
+            },
+            {
+                headerName: `${props.intl.formatMessage({
+                    id: 'component.workflow.datagrid.comments'
+                })}`,
+                field: 'Comments',
+                width: 140
+            },
+        );
 
+        this.state = {
+            pagination: {
+                PageIndex: 1,
+                RowsPerPage: Constantes.DEFAULT_PAGE_SIZE
+            },
+            columnDefs: columnDefs,
+            rowData: [],
+            loadingInformation: false
         };
     }
 
@@ -26,6 +60,32 @@ class WorkflowComponent extends React.Component {
                     <Col>
                         <FormattedMessage id="component.workflow.title" />
                     </Col>
+                </Row>
+                <Row>
+                    <PanelComponent
+                        title={`${this.props.intl.formatMessage({
+                            id: 'materials.panel.materialList.title'
+                        })}`}
+                    >
+                        <DataGridComponent
+                            isLoading={this.state.loadingInformation}
+                            classContainer="grid-container"
+                            onPaginationChange={pagination => {
+                                this.setState(
+                                    {
+                                        pagination: pagination
+                                    },
+                                    () => {
+                                        // search workflows
+                                    }
+                                );
+                            }}
+                            columnDefs={this.state.columnDefs}
+                            rowData={this.state.rowData}
+                            pagination={this.state.pagination}
+                            enableColResize={true}
+                        />
+                    </PanelComponent>
                 </Row>
             </WorkflowContext.Provider>
         );
