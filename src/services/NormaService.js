a8eb0c1acab900b3e78cc6710436febf399cc4a8
Guillmar Ortiz <guillmar.ortiz@gmail.com>
2019-11-07 12:04:58 -0300

    Fix download files

--- src/services/NormaService.js
+++ src/services/NormaService.js
@@ -1,12 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default class NormaService {
-  get = () => axios.get("/norma/");
+  get = () => axios.get('/norma/');
   getById = id => axios.get(`/norma/${id}`);
   publish = id => axios.post(`/norma/publish/${id}`);
   post = (normaId, descripcion) => axios.post(`/norma/${normaId}`, descripcion);
   downloadNormaFile = (normaId, fileType) =>
-    axios.get(`/norma-files/${normaId}/${fileType}`);
+      axios.post(`/norma-files/download/${normaId}/${fileType}`, null, {responseType: 'arraybuffer'});
   uploadNormaFile = (normaId, fileType, formData) =>
-    axios.post(`/norma-files/${normaId}/${fileType}`, formData);
+      axios.post(`/norma-files/upload/${normaId}/${fileType}`, formData);
 }
